121468066b296aa709166592cad4b7044ab86c07
HitzSPB <daniel.oesterbro@gmail.com>
2021-07-02 20:57:14 +0200

    Added if statement to check if sql pod already exist

--- src/Setup-kubernetes.ps1
+++ src/Setup-kubernetes.ps1
@@ -1,1 +1,14 @@
-kubectl create -f $PSScriptRoot/pods/sqlserver.pod.yml --validate=true
+$PodsAvailable = kubectl get pods
+
+if($PodsAvailable -contains "bolig-sql-server") {
+    kubectl create -f $PSScriptRoot/pods/sqlserver.pod.yml
+}
+else {
+    kubectl apply -f $PSScriptRoot/pods/sqlserver.pod.yml
+}
+
+# Log information to check what is running
+
+
+# Write-Host "---"
+# Write-Host $PodsAvailable
